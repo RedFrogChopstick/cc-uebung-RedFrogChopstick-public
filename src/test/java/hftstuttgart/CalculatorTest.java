2f9fa5555fb09701ff6b08a83876cded2e73a778
RedFrogChopstick <max_sauer@gmx.de>
2020-06-08 17:31:18 -0700

    Drone Pipeline && Rest

--- src/test/java/hftstuttgart/CalculatorTest.java
+++ src/test/java/hftstuttgart/CalculatorTest.java
@@ -7,15 +7,15 @@ import org.junit.Test;
 public class CalculatorTest {
     @Test
     public void sumNumbers() {
-        //Calculator calc = new Calculator();
-        //int sum = calc.sum(31, 11);
-        //assertEquals(42, sum);
+        Calculator calc = new Calculator();
+        int sum = calc.sum(31, 11);
+        assertEquals(42, sum);
     }
 
     @Test
     public void prodNumbers() {
-        //Calculator calc = new Calculator();
-        //int prod = calc.prod(3, 7);
-        //assertEquals(21, prod);
+        Calculator calc = new Calculator();
+        int prod = calc.prod(3, 7);
+        assertEquals(21, prod);
     }
 }
